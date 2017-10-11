2a5128cec319a6faea0ad28029847abf5b8fdc25
Ana Palacio <ana.palacioc@gmail.com>
2017-10-11 18:43:20 -0400

    Update IPAM_Import.R

--- IPAM_Import.R
+++ IPAM_Import.R
@@ -1,35 +1,40 @@
 Import_YII<- function (dir, data.files="*.csv")  {
+ 
+# 1. Read all the .csv files (; delimited) and consolidate them in a single data frame
+  # dir: folder containing the .csv files. The data in the dir can be separated in subfolders, recursive = TRUE will find the .csv files inside the subfolders
+  # You can use the subfolders names to later extract information with the option full.names=TRUE, in this 
+  # example the subfolders are the differents sampling dates, but it could be treatments, locations...
 
- # 1. Get multiple .csv files and pool the data
-  
-      IPAM.data<-plyr::ldply (list.files(path=dir, pattern=data.files, 
-                             full.names=TRUE, recursive = TRUE),
+   IPAM.data<-plyr::ldply (list.files(path=dir, pattern=data.files, 
+                                full.names=TRUE, recursive = TRUE),
                      function(filename) {
                        dum=(read.csv(filename, sep = ";", blank.lines.skip=T))
                        dum$filename=filename
                        return(dum)
                      })  
-    
-  
+     
 # 2. Select the columns that contain the file path and the YII values (remove F0 and Fm values)
-  
   IPAM.data<-IPAM.data[,grep("filename|Y.II.", names(IPAM.data))]
   
   # Change to long format
   IPAM.data <- na.omit(reshape2::melt(IPAM.data, id=c("filename")))
   
-# 3.separate folder and file information contained in "filename"
+# 3.If you have subfolders separate folder and file information contained in "filename"
   # In this example each subfolder correspond to different dates, but it could be treatments, locations...
   # Followed for the specific file name
-  
   file.picture <- plyr::rbind.fill(lapply(strsplit(as.character(IPAM.data$filename), split="/"), 
                                     function(X) data.frame(t(X))))
   colnames(file.picture) <- c("Folder", "Date", "File")
   
-  result <- cbind(file.picture[,-1], IPAM.data[,-1])
+  YII <- cbind(file.picture[,-1], IPAM.data[,-1])
   
+# 4. Get and save the ID info as a data frame  
+  ID_AOI<-read.csv("ID_AOI.csv")
+  ID_AOI$variable<-paste("Y.II.",ID_AOI$AOI, sep = "")
   
-# 4. Return final data fraem
-  return(result)
-
-} 
+# 5. Merge YII and sample info (optional)
+  YII.data<-plyr::join(ID_AOI, YII, by =c("Date", "File", "variable"), type="inner")
+  
+  return(YII.data)
+  
+}
